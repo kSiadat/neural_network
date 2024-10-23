ddc8c56451da880ca21c31e99da71ec83bb6edc3
kian <kjs038@student.bham.ac.uk>
2024-10-23 01:52:38 +0100

    replaced old xor files

--- xor.py
+++ xor.py
@@ -1,48 +1,36 @@
 from numpy import absolute, array
 
-from _general.network import Network
+from _general_2.network import Network
 
 
-def evaluate_2(answers, labels):
-        loss = absolute(answers - labels)
-        pass
-
-
-def evaluate(answers, labels):
-        l = len(labels)
-        clean = []
-        messy = []
-        for x in range(l):
-            loss = abs(answers[x][0] - labels[x][0])
-            messy.append(loss)
-            if loss < 0.5:
-                clean.append(1)
-            else:
-                clean.append(0)
-        return sum(clean) / l, round(sum(messy), 5) / l, clean, [round(X, 3)  for X in messy]
-
-#net = Network([2, 2, 1], ["leaky_relu", "sigmoid"])
-net = Network(path="xor")
 data = array([[0, 0], [0, 1], [1, 0], [1, 1]])
-labels = array([[0.1], [0.9], [0.9], [0.1]])
-labels2 = array([[0], [1], [1], [0]])
-training = True
+label = array([[0.1], [0.9], [0.9], [0.1]])
+human_label = array([[0], [1], [1], [0]])
 
-if training:
-    rate = 0.00005
-    epoch_count = 5000000
-    interval = 10000
-    answers = net.epoch_test(data)
-    clean_avg, messy_avg, clean_ind, messy_ind = evaluate(answers, labels2)
-    print("0\t", clean_avg, "\t", messy_avg, "\t", clean_ind, "\t", messy_ind)
-    for x in range(epoch_count):
-        answers = net.epoch(data, labels, rate)
-        if ((x + 1) % interval == 0):
-            clean_avg, messy_avg, clean_ind, messy_ind = evaluate(answers, labels2)
-            print(x + 1, "\t", clean_avg, "\t", messy_avg, "\t", clean_ind, "\t", messy_ind)
+rate = 0.01
+epochs = 10000
+interval = 1000
+
+load = False
+load_path = "xor_auto-save"
+save = True
+save_path = load_path
+
+def print_test(x, data, label):
+    answer = network.epoch_test(data)
+    individual = absolute(answer - human_label)
+    average = individual.sum() / len(data)
+    print(f"{x + 1}:\t{round(average, 4)}\t{[round(X[0], 4)  for X in individual]}")
+
+if load:
+    network = Network(None, None, None, in_path)
 else:
-    answers = net.epoch_test(data)
-    clean_avg, messy_avg, clean_ind, messy_ind = evaluate(answers, labels2)
-    print(clean_avg, "\t", messy_avg, "\t", clean_ind, "\t", messy_ind)
+    network = Network([2, 2, 1], ["relu", "sigmoid"], [0.5, 0.5])
 
-net.save("auto_save")
+print_test(-1, data, human_label)
+for x in range(epochs):
+    network.online_epoch(data, label, rate)
+    if (x + 1) % interval == 0:
+        print_test(x, data, human_label)
+
+network.save(save_path)
