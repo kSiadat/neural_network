7848ca56fc35556482c372da046eae7fb514414a
kian <kjs038@student.bham.ac.uk>
2024-12-04 20:48:08 +0000

    re-ordered and headlined outputs

--- mnist.py
+++ mnist.py
@@ -21,19 +21,19 @@ reader = Mnist_reader(main_path)
 train_img, train_lab, test_img, test_lab = reader.read_all()
 
 # settings
-train = False
+train = True
 
 rate = 0.001
-epochs = 5
+epochs = 10000
 sample_size = 20
 
-interval = 1
-test_interval = 1
+interval = 100
+test_interval = 1000
 test_sample_size = 0
 
-load = True
+load = False
 load_path = "mnist"
-save = False
+save = True
 save_path = "mnist_auto-save"
 # end settings
 
@@ -51,29 +51,31 @@ if train:
         test_loss, test_error = part_test()
     else:
         test_loss, test_error = full_test()
-    print(f"0:\t\t\t{test_loss.mean().round(4)}\t{test_error}")
+    print("epoch\terror\tloss\terror\tloss\tloss")
+    print(f"0:\t\t\t{test_error}\t{test_loss.mean().round(4)}")
 
     for x in range(epochs):
-        #train_answer, indexes = network.batch_epoch(train_img, train_lab, rate, sample_size)
+        train_answer, indexes = network.batch_epoch(train_img, train_lab, rate, sample_size)
         #train_answer = network.online_epoch(train_img, train_lab, rate)
-        train_answer = network.epoch(train_img, train_lab, rate)
+        #train_answer = network.epoch(train_img, train_lab, rate)
         if (x + 1) % interval == 0:
-            #train_loss = network.loss_function(train_lab[indexes], train_answer)
-            #train_error = error_rate(train_lab[indexes], train_answer)
-            train_loss = network.loss_function(train_lab, train_answer)
-            train_error = error_rate(train_lab, train_answer)
+            train_loss = network.loss_function(train_lab[indexes], train_answer)
+            train_error = error_rate(train_lab[indexes], train_answer)
+            #train_loss = network.loss_function(train_lab, train_answer)
+            #train_error = error_rate(train_lab, train_answer)
             if (x + 1) % test_interval == 0:
                 if test_sample_size > 0:
                     test_loss, test_error = part_test()
                 else:
                     test_loss, test_error = full_test()
-                print(f"{x+1}:\t{train_loss.mean().round(4)}\t{train_error.round(4)}\t{test_loss.mean().round(4)}\t{test_error}\t{test_loss.mean(axis=0).round(4)}")
+                print(f"{x+1}:\t{train_error.round(4)}\t{train_loss.mean().round(4)}\t{test_error}\t{test_loss.mean().round(4)}\t{test_loss.mean(axis=0).round(4)}")
             else:
-                print(f"{x+1}:\t{train_loss.mean().round(4)}\t{train_error}")
+                print(f"{x+1}:\t{train_error}\t{train_loss.mean().round(4)}")
 
     if save:
         network.save(save_path)
 
 else:
     loss, error = full_test()
-    print(f"{loss.mean().round(4)}\t{error}\t{loss.mean(axis=0).round(4)}")
+    print("error\tloss\tloss")
+    print(f"{error}\t{loss.mean().round(4)}\t{loss.mean(axis=0).round(4)}")
